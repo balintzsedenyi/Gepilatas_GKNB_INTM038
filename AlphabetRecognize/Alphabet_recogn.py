e459adb6a7456dad03dc0cfb58baff73fec17a83
balintzsedenyi <zsedenyi.balint.adam@gmail.com>
2021-12-30 18:52:20 +0100

    Models

--- AlphabetRecognize/Alphabet_recogn.py
+++ AlphabetRecognize/Alphabet_recogn.py
@@ -14,18 +14,19 @@ for opt, arg in opts:
     if opt == '-h':
         print('Alphabet_recogn.py -i <inputfile>')
         sys.exit()
-    elif opt in ("-i", "--ifile"):
+    elif opt in ("-i", "--infile"):
         inputfile = arg
 
-model = load_model('mymodel.h5')
+model = load_model('98numbonly.h5')
 #model.summary()
 alph_dict = {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9',
              10:'a',11:'b',12:'c',13:'d',14:'e',15:'f',16:'g',17:'h',18:'i',19:'j',
              20:'k',21:'l',22:'m',23:'n',24:'o',25:'p',26:'q',27:'r',28:'s',29:'t',
-             30:'u',31:'v',32:'w',33:'x',34:'y',35:'z',36:'A',37:'B',38:'C',39:'D',
-             40:'E',41:'F',42:'G',43:'H',44:'I',45:'J',46:'K',47:'L',48:'M',49:'N',
-             50:'O',51:'P',52:'Q',53:'R',54:'S',55:'T',56:'U',57:'V',58:'W',59:'X',
-             60:'Y',61:'Z'}
+             30:'u',31:'v',32:'w',33:'x',34:'y',35:'z',
+             36:'A',37:'B',38:'C',39:'D',40:'E',41:'F',42:'G',43:'H',44:'I',45:'J',
+             46:'K',47:'L',48:'M',49:'N',50:'O',51:'P',52:'Q',53:'R',54:'S',55:'T',
+             56:'U',57:'V',58:'W',59:'X',60:'Y',61:'Z'}
+             
 img= cv2.imread(inputfile)
 img_copy =img.copy()
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -33,13 +34,16 @@ img = cv2.resize(img, (400,400))
 img_copy = cv2.GaussianBlur(img_copy, (7,7), 0)
 img_gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
 _, img_thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY_INV)
-img_final = cv2.resize(img_thresh, (128,128))
-img_final =np.reshape(img_final, (1,128,128,1))
+img_final = cv2.resize(img_thresh, (24,24))
+img_final =np.reshape(img_final, (1,24,24,1))
 img_pred = alph_dict[np.argmax(model.predict(img_final))]
+tipp=np.argmax(model.predict(img_final))
+print(tipp)
 print('Number of arguments: {}'.format(len(sys.argv)))
 print('Argument(s) passed: {}'.format(str(sys.argv)))
-cv2.putText(img, "Tipp: " + img_pred, (20,380),cv2.FONT_HERSHEY_DUPLEX, 1.3, color = (255,0,0))
+cv2.putText(img, "Tipp: " + img_pred, (20,370),cv2.FONT_HERSHEY_TRIPLEX, 1.3, color = (255,0,0))
 cv2.imshow('Handwritten character recognition', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+#python Alphabet_recogn.py -i "D:\School things\DigitalVision\trainset\3\train_33_01920.png"
--- AlphabetRecognize/seq_mod.py
+++ AlphabetRecognize/seq_mod.py
@@ -1,16 +1,61 @@
+
+"""
 import os
 import cv2
 import numpy as np
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
+import keras
+from keras import layers
+"""
 
-IMG_WIDTH=128
-IMG_HEIGHT=128
-DATA_DIR=r"D:\School things\DigitalVision\trainset"
-NUM_CLASSES = 62
+from keras.layers.core.dropout import Dropout
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Rescaling
+
+IMG_WIDTH=24
+IMG_HEIGHT=24
+DATA_DIR=r"D:\School things\DigitalVision\trainset1\Szamok"
+NUM_CLASSES = 10
+
+trainset=tf.keras.utils.image_dataset_from_directory(
+    DATA_DIR,
+    labels="inferred",
+    label_mode="categorical",
+    class_names=None,
+    color_mode="grayscale",
+    batch_size=64,
+    image_size=(IMG_WIDTH, IMG_HEIGHT),
+    shuffle=True,
+    seed=123,
+    validation_split=0.2,
+    subset="training",
+    interpolation="bilinear",
+    follow_links=False,
+    crop_to_aspect_ratio=False
+)
+valset=tf.keras.utils.image_dataset_from_directory(
+    DATA_DIR,
+    labels="inferred",
+    label_mode="categorical",
+    class_names=None,
+    color_mode="grayscale",
+    batch_size=64,
+    image_size=(IMG_WIDTH, IMG_HEIGHT),
+    shuffle=True,
+    seed=123,
+    validation_split=0.2,
+    subset="validation",
+    interpolation="bilinear",
+    follow_links=False,
+    crop_to_aspect_ratio=False
+)
+
+"""
+class_names = trainset.class_names
+print(class_names)
 
 label=os.listdir(DATA_DIR)
 filelist=[]
@@ -30,9 +75,12 @@ for i in label:
                 
       cl+=1.0
             
-img_dset = np.array([cv2.imread(fname,0) for fname in filelist ])
+#img_dset = np.array([cv2.imread(fname,0) for fname in filelist ])
+for fname in filelist:
+      img_dset=np.array(cv2.imread(fname,0).astype('float32'))
+img_dset=img_dset/255.0
 
-train_ds, test_ds, train_l, test_l=train_test_split(img_dset, label_s, test_size=0.2)
+train_ds, test_ds, train_l, test_l=train_test_split(dataset, labelset, test_size=0.2)
 
 train_ds = train_ds.reshape(train_ds.shape[0],train_ds.shape[1],train_ds.shape[2],1)
 test_ds = test_ds.reshape(test_ds.shape[0], test_ds.shape[1], test_ds.shape[2],1)
@@ -40,7 +88,20 @@ test_ds = test_ds.reshape(test_ds.shape[0], test_ds.shape[1], test_ds.shape[2],1
 train_lb = to_categorical(train_l, num_classes = NUM_CLASSES, dtype='int')
 test_lb = to_categorical(test_l, num_classes = NUM_CLASSES, dtype='int')
 
+model = keras.Sequential(
+    [
+        keras.Input(shape=(IMG_HEIGHT,IMG_WIDTH,1)),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(NUM_CLASSES, activation="softmax"),
+    ]
+)"""
 model = Sequential()
+model.add(Rescaling(1./255))
 model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(IMG_HEIGHT,IMG_WIDTH,1)))
 model.add(MaxPool2D(pool_size=(2, 2), strides=2))
 model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding = 'same'))
@@ -48,15 +109,17 @@ model.add(MaxPool2D(pool_size=(2, 2), strides=2))
 model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding = 'valid'))
 model.add(MaxPool2D(pool_size=(2, 2), strides=2))
 model.add(Flatten())
+model.add(Dropout(0.5))
 model.add(Dense(64,activation ="relu"))
 model.add(Dense(128,activation ="relu"))
 model.add(Dense(NUM_CLASSES,activation ="softmax"))
-
-
+#model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["sparse_categorical_accuracy"],)
 model.compile(optimizer = Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
-history = model.fit(train_ds, train_lb, epochs=3,  validation_data = (test_ds,test_lb))
+history = model.fit(trainset, batch_size=64, epochs=1,  validation_data = valset)
 model.summary()
-model.save(r'model_test.h5')
+score=model.evaluate(valset,verbose=0)
+print(score)
+model.save(r'98numbonly.h5')
 print("The validation accuracy is :", history.history['val_accuracy'])
 print("The training accuracy is :", history.history['accuracy'])
 print("The validation loss is :", history.history['val_loss'])
